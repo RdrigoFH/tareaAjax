a412c1b895f466e912289076f3f5e9594cee689f
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-05-10 21:32:25 -0500

    Se crearon los arreglos label y dataset para imprimir el grafico en pantalla

--- Problema1/script.js
+++ Problema1/script.js
@@ -12,12 +12,21 @@ function cargarJSON(callback) {
 
 function crearGrafico(jsonData) {
   var data = JSON.parse(jsonData);
-
   var select1 = document.getElementById("provinceSelect1");
   var select2 = document.getElementById("provinceSelect2");
   var province1 = select1.options[select1.selectedIndex].value;
   var province2 = select2.options[select2.selectedIndex].value;
-
   var provinceData1 = data.find(item => item.region === province1);
   var provinceData2 = data.find(item => item.region === province2);
+  var labels = [];
+  var dataset = [];
+  labels.push(province1);
+  labels.push(province2);
+  var confirmed1 = provinceData1.confirmed.map(function(data) {
+    return data.value;
+  });
+  var confirmed2 = provinceData2.confirmed.map(function(data) {
+    return data.value;
+  });
+  
 }
