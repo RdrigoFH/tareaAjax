83fe5abf15ac566038470f5befe59b1b47a2fd48
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-05-12 12:03:59 -0500

    Se creo la funcio get random color que genera colores aleatorios para la representacion de cada grafica y su diferenciacion

--- Problema2/script.js
+++ Problema2/script.js
@@ -1,85 +1,93 @@
 document.addEventListener("DOMContentLoaded", function() {
-   fetch('data.json')
-     .then(response => response.json())
-     .then(data => processData(data))
-     .catch(error => console.error('Error fetching data:', error));
-     
-     function processData(regionsData) {
-      const data = {
-        labels: [],
-        datasets: []
-      };
-  
-      const excludedRegion = "Lima";
-      const excludedRegion2 = "Callao";
-  
-      regionsData.forEach(region => {
-        if (region.region !== excludedRegion && region.region !== excludedRegion2 ) {
-          region.confirmed.forEach(day => {
+  fetch('data.json')
+    .then(response => response.json())
+    .then(data => processData(data))
+    .catch(error => console.error('Error fetching data:', error));
+
+  function processData(regionsData) {
+    const data = {
+      labels: [],
+      datasets: []
+    };
+
+    const excludedRegion = "Lima";
+    const excludedRegion2 = "Callao";
+
+    regionsData.forEach(region => {
+      if (region.region !== excludedRegion && region.region !== excludedRegion2 ) {
+        region.confirmed.forEach(day => {
+          const date = new Date(day.date);
+          const formattedDate = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
+          if (!data.labels.includes(formattedDate)) {
+            data.labels.push(formattedDate);
+          }
+        });
+      }
+    });
+
+    regionsData.forEach(region => {
+      if (region.region !== excludedRegion && region.region !== excludedRegion2) {
+        const dataset = {
+          label: region.region,
+          data: [],
+          borderColor: getRandomColor(),
+          fill: false
+        };
+
+        data.labels.forEach(label => {
+          const matchingDay = region.confirmed.find(day => {
             const date = new Date(day.date);
             const formattedDate = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
-            if (!data.labels.includes(formattedDate)) {
-              data.labels.push(formattedDate);
-            }
+            return formattedDate === label;
           });
-        }
-      });
-      regionsData.forEach(region => {
-        if (region.region !== excludedRegion && region.region !== excludedRegion2) {
-          const dataset = {
-            label: region.region,
-            data: [],
-            borderColor: getRandomColor(),
-            fill: false
-          };
-  
-          data.labels.forEach(label => {
-            const matchingDay = region.confirmed.find(day => {
-              const date = new Date(day.date);
-              const formattedDate = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
-              return formattedDate === label;
-            });
-  
-            if (matchingDay) {
-              dataset.data.push(matchingDay.value);
-            } else {
-              dataset.data.push(0);
+
+          if (matchingDay) {
+            dataset.data.push(matchingDay.value);
+          } else {
+            dataset.data.push(0);
+          }
+        });
+
+        data.datasets.push(dataset);
+      }
+    });
+
+    const ctx = document.getElementById('confirmedChart').getContext('2d');
+    const confirmedChart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            type: 'time',
+            time: {
+              unit: 'day',
+              tooltipFormat: 'MMM DD, YYYY'
+            },
+            scaleLabel: {
+              display: true,
+              labelString: 'Date'
             }
-          });
-  
-          data.datasets.push(dataset);
+          }],
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Confirmed Cases'
+            }
+          }]
         }
-        const ctx = document.getElementById('confirmedChart').getContext('2d');
-     const confirmedChart = new Chart(ctx, {
-       type: 'line',
-       data: data,
-       options: {
-         responsive: true,
-         maintainAspectRatio: false,
-         scales: {
-           xAxes: [{
-             type: 'time',
-             time: {
-               unit: 'day',
-               tooltipFormat: 'MMM DD, YYYY'
-             },
-             scaleLabel: {
-               display: true,
-               labelString: 'Date'
-             }
-           }],
-           yAxes: [{
-             scaleLabel: {
-               display: true,
-               labelString: 'Confirmed Cases'
-             }
-           }]
-         }
-       }
-     });
+      }
+    });
+  }
 
-      });
- 
-   }   
-
-})
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+});
