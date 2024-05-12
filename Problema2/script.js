92d612577f8520669de307f84633d23fba33d3af
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-05-11 21:06:21 -0500

    Se ccreo la subfuncion proces data

--- Problema2/script.js
+++ Problema2/script.js
@@ -4,4 +4,25 @@ document.addEventListener("DOMContentLoaded", function() {
      .then(data => processData(data))
      .catch(error => console.error('Error fetching data:', error));
      
+     function processData(regionsData) {
+      const data = {
+        labels: [],
+        datasets: []
+      };
+  
+      const excludedRegion = "Lima";
+      const excludedRegion2 = "Callao";
+  
+      regionsData.forEach(region => {
+        if (region.region !== excludedRegion && region.region !== excludedRegion2 ) {
+          region.confirmed.forEach(day => {
+            const date = new Date(day.date);
+            const formattedDate = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
+            if (!data.labels.includes(formattedDate)) {
+              data.labels.push(formattedDate);
+            }
+          });
+        }
+      });
+   }   
 })
